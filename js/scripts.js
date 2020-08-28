3fdfae573c32fc57b2de9a5f6fcd0bb46f73b98f
Ben Stoller <stollersystemsound@gmail.com>
2020-08-28 08:16:21 -0700

    add initial html and js constructor

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+// Business Logic 
+
+function Pizza(size) {
+  this.size = size
+  this.toppings = []
+  this.cost = 0
+}
+
+Pizza.prototype.cost = function() {
+  
+}
+
+
+
+
+
+
+
+
+
+
+// User Interface Logic 
+
+$(document).ready(function() {
+
+
+
+});
