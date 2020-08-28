3569b0ae8a5f4b920a3320be1925388c6df23cf2
Ben Stoller <stollersystemsound@gmail.com>
2020-08-28 09:00:26 -0700

    initialize some test toppings

--- js/scripts.js
+++ js/scripts.js
@@ -26,26 +26,24 @@ Pizza.prototype.findCost = function() {
   this.cost = runningCost
 }
 
-let pie1 = new Pizza("Small")
+let testPie = new Pizza("Small")
 
 Pizza.prototype.addTopping = function() {
 
 }
 
+// Toppings
 
-function Toppings(name,cost) {
+function Topping(name,cost,id) {
   this.name = name
   this.cost = cost
+  this.id = id
 }
 
-
-
-
-
-
-
-
-
+let cheese = new Topping("Cheese",2.50,"1")
+let pepperoni = new Topping("Pepperoni",1,"2")
+let olives = new Topping("Olives",.25,"3")
+let onions = new Topping("Onions",.25,"4")
 
 
 
