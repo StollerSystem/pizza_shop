e6ba61d6bd6ec7277c1ef412cc35cacec9ca7e77
Ben Stoller <stollersystemsound@gmail.com>
2020-08-28 08:35:25 -0700

    add initial js for cost prototype

--- js/scripts.js
+++ js/scripts.js
@@ -6,9 +6,39 @@ function Pizza(size) {
   this.cost = 0
 }
 
-Pizza.prototype.cost = function() {
-  
+Pizza.prototype.findCost = function() {
+  let runningCost = 0
+  const size = this.size
+  switch (size) {
+    case ("Small"):
+      console.log("small");
+      runningCost += 10
+      break;
+    case ("Medium"):
+      console.log("medium");
+      runningCost += 15
+      break;
+    case ("Large"):
+      console.log("large");
+      runningCost = 20 
+      break;
+  } 
+  this.cost = runningCost
 }
+
+let pie1 = new Pizza("Small")
+
+Pizza.prototype.addTopping = function() {
+
+}
+
+
+function Toppings(name,cost) {
+  this.name = name
+  this.cost = cost
+}
+
+
 
 
 
